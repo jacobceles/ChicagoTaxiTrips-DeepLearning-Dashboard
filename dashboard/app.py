872dfe9b40fd59822f56b6fb63fa246a2f118e7a
Jacob Celestine <jacobceles@gmail.com>
2021-12-03 13:29:03 -0500

    Added chart support
    Added chart js in index.html
    Added formatting and multi model support in flask app

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,13 +1,33 @@
-from flask import Flask, request
-from flask import render_template
+import keras
+import calendar
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
+models = {
+    "2015": keras.models.load_model("NN_model"),
+    "2016": keras.models.load_model("NN_model"),
+    "2017": keras.models.load_model("NN_model"),
+    "2018": keras.models.load_model("NN_model"),
+    "2019": keras.models.load_model("NN_model"),
+    "2020": keras.models.load_model("NN_model"),
+    "2021": keras.models.load_model("NN_model")
+}
 
 
-@app.route('/updateData', methods=['POST'])
+@app.route('/prediction', methods=['POST', 'GET'])
 def get_prediction():
-    a = request.form['labels0']
-    return "success", 201
+    month = request.args.get('month', default=2, type=int)
+    seconds = request.args.get('seconds', default=50.0, type=float)
+    distance = request.args.get('distance', default=50.0, type=float)
+    fares = []
+    years = ["2015", "2016", "2017", "2018", "2019", "2020", "2021"]
+    for model in models.values():
+        fares.append(str(model.predict([[seconds, distance, month]])[0][0]))
+    """
+    result = "In the month of {0}, if you rode a taxi in Chicago for {1} minutes to cover a distance of {2} miles, it" \
+             " would have cost you ${3}!".format(calendar.month_name[month], seconds/60, distance, str(round(fare, 2)))
+    """
+    return jsonify(labels=years, values=fares)
 
 
 @app.route("/")
